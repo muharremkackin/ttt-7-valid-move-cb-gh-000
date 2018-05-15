1399f2dff25fc60a66f76712778f230d684e524d
Muharrem Kaçkın <muharremkackin@gmail.com>
2018-05-15 01:27:05 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 # code your #valid_move? method here
-def valid_move?(board, index.to_i)
+def valid_move?(board, index)
   if index < 9 && index > -1
     if(position_taken?(board, index) == true)
       return true
@@ -8,7 +8,7 @@ def valid_move?(board, index.to_i)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, index.to_i)
+def position_taken?(board, index)
   if board[index] == " "
     return true
   elsif board[index] == ""
